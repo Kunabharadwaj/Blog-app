674e6db7eb6c0f69b5172b4d014b961c1eaa0583
Bharadwaj <kunabharadwaj520@gmail.com>
2022-12-22 20:34:04 +0000

    register page is done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+ 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>Register</h1>
+      <form action="">
+        <input required type="text" placeholder='username'/>
+        <input required type="text" placeholder='password'/>
+        <input required type="text" placeholder='email'/>
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>Do you have an account? <Link to='/Login'>Login</Link></span>
+      </form>
+
+    </div>
   )
 }
 
